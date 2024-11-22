38abf5d3f93dfcf964e17a3c33337bac4622ca31
Maybeiley <2784519@gmail.com>
2024-11-22 18:17:14 +0900

    Refactor: Apply asyncHandler and modify errorHandler

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -7,55 +7,43 @@ export class AuthController {
 	}
 
 	createUser = async (req, res, next) => {
-		try {
-			const user = await this.service.createUser(req.body);
-			return res.status(HttpStatus.CREATED).json(user);
-		} catch (error) {
-			next(error);
-		}
+		const user = await this.service.createUser(req.body);
+		return res.status(HttpStatus.CREATED).json(user);
 	};
 
 	getUser = async (req, res, next) => {
 		const { email, password } = req.body;
 
-		try {
-			const user = await this.service.getUser(email, password);
-			const accessToken = await this.service.createToken(user);
-			const refreshToken = await this.service.createToken(user, 'refresh');
-			await this.service.updateUser(user.id, { refreshToken });
+		const user = await this.service.getUser(email, password);
+		const accessToken = await this.service.createToken(user);
+		const refreshToken = await this.service.createToken(user, 'refresh');
+		await this.service.updateUser(user.id, { refreshToken });
 
-			res.cookie('refreshToken', refreshToken, {
-				path: '/token/refresh',
-				httpOnly: true,
-				sameSite: 'none',
-				secure: true,
-				maxAge: 14 * 24 * 60 * 60 * 1000, // 14일
-			});
+		res.cookie('refreshToken', refreshToken, {
+			path: '/token/refresh',
+			httpOnly: true,
+			sameSite: 'none',
+			secure: true,
+			maxAge: 14 * 24 * 60 * 60 * 1000, // 14일
+		});
 
-			return res.json({ accessToken });
-		} catch (error) {
-			return next(error);
-		}
+		return res.json({ accessToken });
 	};
 
 	getNewToken = async (req, res, next) => {
-		try {
-			const { refreshToken } = req.cookies;
-			const { userId } = req.auth;
-			const { accessToken, newRefreshToken } = await this.service.refreshToken(userId, refreshToken);
-			await this.service.updateUser(userId, { refreshToken: newRefreshToken });
+		const { refreshToken } = req.cookies;
+		const { userId } = req.auth;
+		const { accessToken, newRefreshToken } = await this.service.refreshToken(userId, refreshToken);
+		await this.service.updateUser(userId, { refreshToken: newRefreshToken });
 
-			res.cookie('refreshToken', newRefreshToken, {
-				path: '/token/refresh',
-				httpOnly: true,
-				sameSite: 'none',
-				secure: true,
-				maxAge: 14 * 24 * 60 * 60 * 1000, // 14일
-			});
+		res.cookie('refreshToken', newRefreshToken, {
+			path: '/token/refresh',
+			httpOnly: true,
+			sameSite: 'none',
+			secure: true,
+			maxAge: 14 * 24 * 60 * 60 * 1000, // 14일
+		});
 
-			return res.json({ accessToken });
-		} catch (error) {
-			return next(error);
-		}
+		return res.json({ accessToken });
 	};
 }
--- src/controllers/shop.controller.ts
+++ src/controllers/shop.controller.ts
@@ -9,18 +9,14 @@ export class ShopController {
 	getShopList = async (req, res) => {
 		const { page = 1, pageSize = 10, orderBy = 'priceLowest', keyword = '', filter } = req.query;
 
-		try {
-			const shops = await this.service.getShopList({
-				page,
-				pageSize,
-				orderBy,
-				keyword: decodeURIComponent(keyword).trim(),
-				filter,
-			});
-			res.status(HttpStatus.SUCCESS).json(shops);
-		} catch (error) {
-			res.status(HttpStatus.SERVER_ERROR).json({ error: error.message });
-		}
+		const shops = await this.service.getShopList({
+			page,
+			pageSize,
+			orderBy,
+			keyword: decodeURIComponent(keyword).trim(),
+			filter,
+		});
+		res.status(HttpStatus.SUCCESS).json(shops);
 	};
 
 	createShop = async (req, res) => {
@@ -39,43 +35,31 @@ export class ShopController {
 	getShopById = async (req, res) => {
 		const { shopId } = req.params;
 
-		try {
-			const shop = await this.service.getShopById(shopId);
-			if (!shop) {
-				throw new Error('해당 판매 정보를 찾을 수 없습니다');
-			}
-			res.status(HttpStatus.SUCCESS).json(shop);
-		} catch (error) {
-			res.status(HttpStatus.NOT_FOUND).json({ error: error.message });
+		const shop = await this.service.getShopById(shopId);
+		if (!shop) {
+			throw new Error('해당 판매 정보를 찾을 수 없습니다');
 		}
+		res.status(HttpStatus.SUCCESS).json(shop);
 	};
 
 	updateShop = async (req, res) => {
 		const { shopId } = req.params;
 
-		try {
-			const { userId } = req.auth;
-			const shop = await this.service.updateShop(shopId, { ...req.body, sellerId: userId });
-			if (!shop) {
-				return res.status(HttpStatus.NOT_FOUND).json({ error: '해당 판매 정보를 찾을 수 없습니다' });
-			}
-			res.status(HttpStatus.SUCCESS).json(shop);
-		} catch (error) {
-			res.status(HttpStatus.SERVER_ERROR).json({ error: error.message });
+		const { userId } = req.auth;
+		const shop = await this.service.updateShop(shopId, { ...req.body, sellerId: userId });
+		if (!shop) {
+			return res.status(HttpStatus.NOT_FOUND).json({ error: '해당 판매 정보를 찾을 수 없습니다' });
 		}
+		res.status(HttpStatus.SUCCESS).json(shop);
 	};
 
 	deleteShop = async (req, res) => {
 		const { shopId } = req.params;
 
-		try {
-			const shop = await this.service.deleteShop(shopId);
-			if (!shop) {
-				return res.status(HttpStatus.NOT_FOUND).json({ message: '해당 판매 정보를 찾을 수 없습니다' });
-			}
-			res.status(HttpStatus.NO_CONTENT).json(shop);
-		} catch (error) {
-			res.status(HttpStatus.BAD_REQUEST).json({ error: error.message });
+		const shop = await this.service.deleteShop(shopId);
+		if (!shop) {
+			return res.status(HttpStatus.NOT_FOUND).json({ message: '해당 판매 정보를 찾을 수 없습니다' });
 		}
+		res.status(HttpStatus.NO_CONTENT).json(shop);
 	};
 }
