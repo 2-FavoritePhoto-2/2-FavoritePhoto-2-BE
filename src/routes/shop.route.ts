1ccb9032da2324c9925a123ce99959c524fb10b5
Maybeiley <2784519@gmail.com>
2024-11-26 13:45:31 +0900

    Fix: Add asyncHandler

--- src/routes/shop.route.ts
+++ src/routes/shop.route.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { shopController } from '../containers/shop.container.js';
 import { verifyAccessToken, verifySeller } from '../utils/verifyAuth.js';
+import asyncHandler from '../utils/asyncHandler.js';
 
 export const shopRouter = express.Router();
 
-shopRouter.get('/', shopController.getShopList);
-shopRouter.post('/', verifyAccessToken, shopController.createShop);
-shopRouter.get('/:shopId', shopController.getShopById);
-shopRouter.patch('/:shopId', verifyAccessToken, verifySeller, shopController.updateShop);
-shopRouter.delete('/:shopId', verifyAccessToken, verifySeller, shopController.deleteShop);
+shopRouter.get('/', asyncHandler(shopController.getShopList));
+shopRouter.post('/', verifyAccessToken, asyncHandler(shopController.createShop));
+shopRouter.get('/:shopId', asyncHandler(shopController.getShopById));
+shopRouter.patch('/:shopId', verifyAccessToken, verifySeller, asyncHandler(shopController.updateShop));
+shopRouter.delete('/:shopId', verifyAccessToken, verifySeller, asyncHandler(shopController.deleteShop));
