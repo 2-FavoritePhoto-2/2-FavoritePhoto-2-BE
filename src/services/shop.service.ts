07b52ac4b4c13e0ad296f22e20f3040eceedc99c
Maybeiley <2784519@gmail.com>
2024-11-26 18:41:45 +0900

    Feat: Add createPurchase API

--- src/services/shop.service.ts
+++ src/services/shop.service.ts
@@ -1,3 +1,5 @@
+import { prismaClient } from '../connection/connection.js';
+
 export class ShopService {
 	data: any;
 	constructor(shopRepository) {
@@ -32,5 +34,44 @@ export class ShopService {
 		const shop = await this.data.deleteShop(id);
 
 		return shop;
+	};
+
+	createPurchase = async (shopId: string, buyerId: string, quantity: number, totalPrice: number) => {
+		const newPurchase = await prismaClient.$transaction(async () => {
+			const shop = await this.data.getShopById(shopId);
+
+			if (!shop || shop.remainingQuantity < quantity) {
+				throw new Error('구매 가능한 수량이 부족합니다!');
+			}
+
+			const usePoints = await this.data.updateUser(buyerId, totalPrice);
+
+			const purchase = await this.data.purchase({
+				quantity,
+				totalPrice,
+				buyerId,
+				shopId,
+			});
+
+			const updatedQuantity = shop.remainingQuantity - quantity;
+			await this.data.updateShop(shopId, { remainingQuantity: updatedQuantity, available: updatedQuantity > 0 });
+
+			const newCardData = {
+				uploaderId: shop.card?.uploaderId,
+				ownerId: buyerId,
+				name: shop.card?.name,
+				price: shop.card?.price,
+				grade: shop.card?.grade,
+				type: shop.card?.type,
+				description: shop.card?.description,
+				image: shop.card?.image,
+				quantity,
+			};
+			await this.data.createPurchasedCard(newCardData);
+
+			return purchase;
+		});
+
+		return newPurchase;
 	};
 }
