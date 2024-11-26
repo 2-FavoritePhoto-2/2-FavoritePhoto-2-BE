1e507b219328cce53495a6983124d1cc03459248
Maybeiley <2784519@gmail.com>
2024-11-26 10:19:12 +0900

    Fix: Remove module

--- src/routes/shop.route.ts
+++ src/routes/shop.route.ts
@@ -1,12 +1,11 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
 import { shopController } from '../containers/shop.container.js';
 import { verifyAccessToken, verifySeller } from '../utils/verifyAuth.js';
 
 export const shopRouter = express.Router();
 
-shopRouter.get('/', asyncHandler(shopController.getShopList));
-shopRouter.post('/', verifyAccessToken, asyncHandler(shopController.createShop));
-shopRouter.get('/:shopId', asyncHandler(shopController.getShopById));
-shopRouter.patch('/:shopId', verifyAccessToken, verifySeller, asyncHandler(shopController.updateShop));
-shopRouter.delete('/:shopId', verifyAccessToken, verifySeller, asyncHandler(shopController.deleteShop));
+shopRouter.get('/', shopController.getShopList);
+shopRouter.post('/', verifyAccessToken, shopController.createShop);
+shopRouter.get('/:shopId', shopController.getShopById);
+shopRouter.patch('/:shopId', verifyAccessToken, verifySeller, shopController.updateShop);
+shopRouter.delete('/:shopId', verifyAccessToken, verifySeller, shopController.deleteShop);
