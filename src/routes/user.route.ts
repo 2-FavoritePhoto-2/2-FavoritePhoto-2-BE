67ce787036a97b66aebf4159cb439fded6c194f6
ciin1411 <ciin1411@naver.com>
2024-11-28 10:10:09 +0900

    feat: add asynchandler

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,17 +1,18 @@
 import express from 'express';
 import { userController } from '../containers/user.container.js';
 import { verifyAccessToken } from '../utils/verifyAuth.js';
+import asyncHandler from '../utils/asyncHandler.js';
 
 export const userRouter = express.Router();
 
 // 유저 프로필 가져오기
-userRouter.get('/profile', verifyAccessToken, userController.getUserProfile);
+userRouter.get('/profile', verifyAccessToken, asyncHandler(userController.getUserProfile));
 
 // 유저 포토 카드 전체 조회
-userRouter.get('/cards', verifyAccessToken, userController.getUserPhotoCards);
+userRouter.get('/cards', verifyAccessToken, asyncHandler(userController.getUserPhotoCards));
 
 // 유저 포토 카드 상세 조회
-userRouter.get('/cards/:cardId', verifyAccessToken, userController.getPhotoCardDetails);
+userRouter.get('/cards/:cardId', verifyAccessToken, asyncHandler(userController.getPhotoCardDetails));
 
 // 거래 제안 목록 조회
-userRouter.get('/exchanges/:shopId', verifyAccessToken, userController.getExchangesByShopId);
+userRouter.get('/exchanges/:shopId', verifyAccessToken, asyncHandler(userController.getExchangesByShopId));
