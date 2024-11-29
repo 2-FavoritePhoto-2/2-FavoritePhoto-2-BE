327d56fd7153a36b4c91ac23b93e92934f9d12df
ciin1411 <ciin1411@naver.com>
2024-11-29 17:44:21 +0900

    refactor: remove comment

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -69,28 +69,23 @@ export class UserController {
 
 	// GET /user/exchanges/:shopId
 	createPhotoCard = async (req, res) => {
-		const ownerId = req.auth.userId; // 인증된 사용자 ID (JWT나 세션에서 가져옴)
+		const ownerId = req.auth.userId;
 		const { name, price, grade, quantity, type, description } = req.body;
 
-		// 파일이 없는 경우 오류 반환
 		if (!req.file) {
 			return res.status(HttpStatus.BAD_REQUEST).json({ error: '이미지를 업로드해야 합니다.' });
 		}
 
-		// 파일 크기 초과 오류 처리
 		if (req.fileValidationError) {
 			return res.status(HttpStatus.BAD_REQUEST).json({ error: req.fileValidationError });
 		}
 
-		// type 유효성 검사 및 파싱
 		try {
 			let parsedType;
 
-			// type이 배열로 직접 들어오면 그대로 사용
 			if (Array.isArray(type)) {
 				parsedType = type;
 			} else {
-				// JSON 문자열로 들어오면 JSON 파싱
 				try {
 					parsedType = JSON.parse(type);
 				} catch (error) {
@@ -100,18 +95,16 @@ export class UserController {
 				}
 			}
 
-			// type이 배열인지, 1개 이상 2개 이하인지 체크
 			if (!Array.isArray(parsedType) || parsedType.length < 1 || parsedType.length > 2) {
 				return res.status(HttpStatus.BAD_REQUEST).json({
 					error: 'type 필드는 최소 1가지, 최대 2가지를 선택해야 합니다.',
 				});
 			}
 
-			const imageUrl = req.file.location; // S3에서 업로드된 이미지 URL 가져오기
+			const imageUrl = req.file.location;
 
-			// 카드 생성
 			const card = await this.service.createPhotoCard({
-				ownerId, // ownerId를 카드 생성에 포함시킴
+				ownerId,
 				name,
 				price: parseInt(price, 10),
 				grade,
@@ -121,7 +114,6 @@ export class UserController {
 				image: imageUrl,
 			});
 
-			// 카드 생성 완료 후 응답
 			res.status(HttpStatus.CREATED).json(card);
 		} catch (error) {
 			res.status(HttpStatus.SERVER_ERROR).json({ error: error.message });
