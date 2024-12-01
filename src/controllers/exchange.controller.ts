40dd6e33cf9d2386c68cba53596dfa7ee181b7fb
Kang Myoung Gon <jin04053@naver.com>
2024-12-01 22:02:23 +0900

    Refactor:  Replace try-catch with asyncHandler in Exchange

--- src/controllers/exchange.controller.ts
+++ src/controllers/exchange.controller.ts
@@ -1,63 +1,47 @@
 import HttpStatus from '../utils/httpStatus.js';
 
 export class ExchangeController {
-	service: any;
-	constructor(exchangeService) {
-		this.service = exchangeService;
-	}
+  service: any;
+  constructor(exchangeService) {
+    this.service = exchangeService;
+  }
 
-	createExchange = async (req, res) => {
-		const { shopId } = req.params;
-		const { buyerCardId, description } = req.body;
-		const { userId } = req.auth;
+  createExchange = async (req, res) => {
+    const { shopId } = req.params;
+    const { buyerCardId, description } = req.body;
+    const { userId } = req.auth;
 
-		try {
-			const newExchange = await this.service.createExchange({
-				shopId,
-				buyerId: userId,
-				buyerCardId,
-				description,
-			});
-			return res.status(HttpStatus.CREATED).json(newExchange);
-		} catch (error) {
-			res.status(HttpStatus.SERVER_ERROR).json({ message: '교환 제안 생성에 실패했습니다.', error: error.message });
-		}
-	};
+    const newExchange = await this.service.createExchange({
+      shopId,
+      buyerId: userId,
+      buyerCardId,
+      description,
+    });
+    return res.status(HttpStatus.CREATED).json(newExchange);
+  };
 
-	acceptExchange = async (req, res) => {
-		const { exchangeId } = req.params;
-		const { userId: sellerId } = req.auth;
+  acceptExchange = async (req, res) => {
+    const { exchangeId } = req.params;
+    const { userId: sellerId } = req.auth;
 
-		try {
-			const approvedExchange = await this.service.acceptExchange(exchangeId, sellerId);
-			return res.status(HttpStatus.SUCCESS).json(approvedExchange);
-		} catch (error) {
-			res.status(HttpStatus.SERVER_ERROR).json({ message: error.message || '서버 오류가 발생했습니다.', details: error.stack });
-		}
-	};
+    const approvedExchange = await this.service.acceptExchange(exchangeId, sellerId);
+    return res.status(HttpStatus.SUCCESS).json(approvedExchange);
+  };
 
-	refuseExchange = async (req, res) => {
-		const { exchangeId } = req.params;
-		const { userId: sellerId } = req.auth;
+  refuseExchange = async (req, res) => {
+    const { exchangeId } = req.params;
+    const { userId: sellerId } = req.auth;
 
-		try {
-			const deletedExchange = await this.service.refuseExchange(exchangeId, sellerId);
-			return res.status(HttpStatus.NO_CONTENT).json({ message: '교환 제안을 거절했습니다.', data: deletedExchange });
-		} catch (error) {
-			res.status(HttpStatus.SERVER_ERROR).json({ message: '교환 제안을 거절할 수 없습니다.', error: error.message });
-		}
-	};
+    const deletedExchange = await this.service.refuseExchange(exchangeId, sellerId);
+    return res.status(HttpStatus.NO_CONTENT).json({ message: '교환 제안을 거절했습니다.', data: deletedExchange });
+  };
 
-	cancelExchange = async (req, res) => {
-		const { exchangeId } = req.params;
-		const { userId: buyerId } = req.auth;
+  cancelExchange = async (req, res) => {
+    const { exchangeId } = req.params;
+    const { userId: buyerId } = req.auth;
 
-		try {
-			const deletedExchange = await this.service.cancelExchange(exchangeId, buyerId);
+    const deletedExchange = await this.service.cancelExchange(exchangeId, buyerId);
 
-			return res.status(HttpStatus.NO_CONTENT).json({ message: '교환 제안을 취소했습니다.', data: deletedExchange });
-		} catch (error) {
-			res.status(HttpStatus.SERVER_ERROR).json({ message: '교환 제안을 취소할 수 없습니다.', error: error.message });
-		}
-	};
+    return res.status(HttpStatus.NO_CONTENT).json({ message: '교환 제안을 취소했습니다.', data: deletedExchange });
+  };
 }
--- src/routes/exchange.route.ts
+++ src/routes/exchange.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { exchangeController } from '../containers/exchange.container.js';
 import { verifyAccessToken } from '../utils/verifyAuth.js';
+import asyncHandler from '../utils/asyncHandler.js';
 
 export const exchangeRouter = express.Router();
 
-exchangeRouter.post('/:shopId/exchange', verifyAccessToken, exchangeController.createExchange);
-exchangeRouter.post('/exchange/:exchangeId/accept', verifyAccessToken, exchangeController.acceptExchange);
-exchangeRouter.delete('/exchange/:exchangeId/refuse', verifyAccessToken, exchangeController.refuseExchange);
-exchangeRouter.delete('/exchange/:exchangeId/cancel', verifyAccessToken, exchangeController.cancelExchange);
+exchangeRouter.post('/:shopId/exchange', verifyAccessToken, asyncHandler(exchangeController.createExchange));
+exchangeRouter.post('/exchange/:exchangeId/accept', verifyAccessToken, asyncHandler(exchangeController.acceptExchange));
+exchangeRouter.delete('/exchange/:exchangeId/refuse', verifyAccessToken, asyncHandler(exchangeController.refuseExchange));
+exchangeRouter.delete('/exchange/:exchangeId/cancel', verifyAccessToken, asyncHandler(exchangeController.cancelExchange));
